dc2eeb8e7aeff81554d60181d2c59f7cc1a44afd
Carson Johnson <carson74johnson@gmail.com>
2021-10-17 17:36:54 -0400

    changed file structure

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const fs = require('fs');
 const path = require('path');
 const inquirer = require('inquirer');
-const generateMarkdown = require('./utils/generateMarkdown');
+const generateMarkdown = require('./generateMarkdown');
 
 // TODO: Create an array of questions for user input
 const questions = [
