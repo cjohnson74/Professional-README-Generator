9b830e7c523943f92b749da6e3e10b5b648a7d17
Carson Johnson <carson74johnson@gmail.com>
2021-10-17 16:43:13 -0400

    added changes to the generated readme

--- Develop/index.js
+++ Develop/index.js
@@ -62,42 +62,58 @@ const questions = [
     //     message: "How many collaborrators helped on this project?"
     // },
     {
-        type: "input",
-        name: "collaborators",
-        message: "List your collaborator names seperated by a comma:"
-    },
-    {
         type: "list",
         name: "license",
         message: "What licnese would you like for this project?",
-        choices: ["MIT License", "GNU AGPLv3", "GNU GPLv3", "GNU LGPLv3", "Mozilla Public License 2.0", "Apache License 2.0", "Boost Software License 1.0", "The Unlicense", "No License"]
+        choices: ["Apache 2.0 License", 
+                    "Boost Software License 1.0", 
+                    "BSD 3-Clause License", 
+                    "BSD 2-Clause License", 
+                    "Eclipse Public License 1.0", 
+                    "GNU GPL v3", "GNU GPL v2",
+                    "GNU AGPL v3",
+                    "GNU LGPL v3",
+                    "GNU FDL v1.3",
+                    "IBM Public License Version 1.0",
+                    "ISC License (ISC)",
+                    "The MIT License",
+                    "Mozilla Public License 2.0",
+                    "Attribution License (BY)",
+                    "Open Database License (ODbL)",
+                    "Public Domain Dedication and License (PDDL)",
+                    "The Perl License",
+                    "The Artistic License 2.0",
+                    "SIL Open Font License 1.1",
+                    "No License"]
     },
     {
         type: "input",
-        name: "features",
-        message: "List your project features here:"
+        name: "tests",
+        message: "Provide examples on how to run your tests: "
     },
     {
         type: "input",
         name: "contribute",
         message: "How can developers contribute to your project?"
-    },
+    }
 ];
 
 async function init() {
     let answers = await inquirer.prompt(questions)
     .then((answers) => {
         console.log(answers);
-    var parsedData = JSON.stringify(answers);
-    console.log(par);
-    writeToFile("README.md", parsedData);
+        // var parsedData = JSON.stringify(answers);
+        // console.log(par);
+        var data = generateMarkdown(answers);
+        JSON.stringify(data)
+        writeToFile("README.md", data);
     })
 }
 
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    return fs.writeFileSync(path.join(process.cwd(), ))
+    return fs.writeFileSync(fileName, data);
 }
 
 // TODO: Create a function to initialize app
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,42 +1,187 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  switch (license) {
+    case "Apache 2.0 License":
+      return "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](#license)";
+      break;
+    case "Boost Software License 1.0":
+      return "[![License](https://img.shields.io/badge/License-Boost%201.0-lightblue.svg)](#license)";
+      break;
+    case "BSD 3-Clause License":
+      return "[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](#license)";
+      break;
+    case "BSD 2-Clause License":
+      return "[![License](https://img.shields.io/badge/License-BSD%202--Clause-orange.svg)](#license)";
+      break;
+    case "Eclipse Public License 1.0":
+      return "[![License](https://img.shields.io/badge/License-EPL%201.0-red.svg)](#license)";
+      break;
+    case "GNU GPL v3":
+      return "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](#license)";
+      break;
+    case "GNU GPL v2":
+      return "[![License: GPL v2](https://img.shields.io/badge/License-GPL%20v2-blue.svg)](#license)";
+      break;
+    case "GNU AGPL v3":
+      return "[![License: AGPL v3](https://img.shields.io/badge/License-AGPL%20v3-blue.svg)](#license)";
+      break;
+    case "GNU LGPL v3":
+      return "[![License: LGPL v3](https://img.shields.io/badge/License-LGPL%20v3-blue.svg)](#license)";
+      break;
+    case "GNU FDL v1.3":
+      return "[![License: FDL 1.3](https://img.shields.io/badge/License-FDL%20v1.3-blue.svg)](#license)";
+      break;
+    case "IBM Public License Version 1.0":
+      return "[![License: IPL 1.0](https://img.shields.io/badge/License-IPL%201.0-blue.svg)](#license)";
+      break;
+    case "ISC License (ISC)":
+      return "[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](#license)";
+      break;
+    case "The MIT License":
+      return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](#license)";
+      break;
+    case "Mozilla Public License 2.0":
+      return "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](#license)";
+      break;
+    case "Attribution License (BY)":
+      return "[![License: Open Data Commons Attribution](https://img.shields.io/badge/License-ODC_BY-brightgreen.svg)](#license)";
+      break;
+    case "Open Database License (ODbL)":
+      return "[![License: ODbL](https://img.shields.io/badge/License-ODbL-brightgreen.svg)](#license)";
+      break;
+    case "Public Domain Dedication and License (PDDL)":
+      return "[![License: ODbL](https://img.shields.io/badge/License-PDDL-brightgreen.svg)](#license)";
+      break;
+    case "The Perl License":
+      return "[![License: Artistic-2.0](https://img.shields.io/badge/License-Perl-0298c3.svg)](#license)";
+      break;
+    case "The Artistic License 2.0":
+      return "[![License: Artistic-2.0](https://img.shields.io/badge/License-Artistic%202.0-0298c3.svg)](#license)";
+      break;
+    case "SIL Open Font License 1.1":
+      return "[![License: Open Font-1.1](https://img.shields.io/badge/License-OFL%201.1-lightgreen.svg)](#license)";
+      break;
+    case "No License":
+      return "";
+      break;
+    default:
+      return "";
+      break;
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  switch (license) {
+    case "Apache 2.0 License":
+      return "https://opensource.org/licenses/Apache-2.0";
+      break;
+    case "Boost Software License 1.0":
+      return "https://www.boost.org/LICENSE_1_0.txt";
+      break;
+    case "BSD 3-Clause License":
+      return "https://opensource.org/licenses/BSD-3-Clause";
+      break;
+    case "BSD 2-Clause License":
+      return "https://opensource.org/licenses/BSD-2-Clause";
+      break;
+    case "Eclipse Public License 1.0":
+      return "https://opensource.org/licenses/EPL-1.0";
+      break;
+    case "GNU GPL v3":
+      return "https://www.gnu.org/licenses/gpl-3.0";
+      break;
+    case "GNU GPL v2":
+      return "https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html";
+      break;
+    case "GNU AGPL v3":
+      return "https://www.gnu.org/licenses/agpl-3.0";
+      break;
+    case "GNU LGPL v3":
+      return "https://www.gnu.org/licenses/lgpl-3.0";
+      break;
+    case "GNU FDL v1.3":
+      return "https://www.gnu.org/licenses/fdl-1.3";
+      break;
+    case "IBM Public License Version 1.0":
+      return "https://opensource.org/licenses/IPL-1.0";
+      break;
+    case "ISC License (ISC)":
+      return "https://opensource.org/licenses/ISC";
+      break;
+    case "The MIT License":
+      return "https://opensource.org/licenses/MIT";
+      break;
+    case "Mozilla Public License 2.0":
+      return "https://opensource.org/licenses/MPL-2.0";
+      break;
+    case "Attribution License (BY)":
+      return "https://opendatacommons.org/licenses/by/";
+      break;
+    case "Open Database License (ODbL)":
+      return "https://opendatacommons.org/licenses/odbl/";
+      break;
+    case "Public Domain Dedication and License (PDDL)":
+      return "https://opendatacommons.org/licenses/pddl/";
+      break;
+    case "The Perl License":
+      return "https://opensource.org/licenses/Artistic-2.0";
+      break;
+    case "The Artistic License 2.0":
+      return "https://opensource.org/licenses/Artistic-2.0";
+      break;
+    case "SIL Open Font License 1.1":
+      return "https://opensource.org/licenses/OFL-1.1";
+      break;
+    case "No License":
+      return "";
+      break;
+    default:
+      return "";
+      break;
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  let licenseBadge = renderLicenseBadge(license);
+  let licenseLink = renderLicenseLink(license);
+  return `## License
+  License Information: ${renderLicenseLink(license)}`;
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  ${renderLicenseBadge(data.license)}
   ## Description
-  Provide a short description explaining the what, why, and how of your project. Use the following questions as a guide:
-  - Motivation: ${data.motivation}
-  - Why was this project built? ${data.why}
-  - What problem does this project solve? ${data.problem}
-  - What I learn? ${data.learn}
-  ## Table of Contents:
+  - Motivation to make this project: ${data.motivation}
+  - Why this project built: ${data.why}
+  - The problem(s) this project solves: ${data.problem}
+  - What I learned: ${data.learn}
+  ## Table of Contents
   - [Installation](#installation)
   - [Usage](#usage)
-  - [Credits](#credits)
   - [License](#license)
-  - [Features](#features)
+  - [Constributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
   ## Installation
   ${data.installation}
   ## Usage
   ${data.usage}
-  ## Credits
-  ${data.collaborators}
-  ## License
-  ${data.license}
-  ## Features
-  ${data.features}
-  ## How to Contribute
-  ${data.contribute}`;
+  ${renderLicenseSection(data.license)}
+  ## Contributing
+  ${data.contribute}
+  ## Tests
+  ${data.tests}
+  ## Questions
+  Send any questions to my Github or Email below. \n
+  Github: https://github.com/${data.github} \n
+  Email: ${data.email}`;
 }
 
 module.exports = generateMarkdown;
